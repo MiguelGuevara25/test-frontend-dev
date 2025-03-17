738bd8fa4eba042573863298eb33135c1a7dbdf7
MiguelGuevara25 <miguel_g1789@hotmail.com>
2025-03-17 10:50:53 -0500

    login completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
+"use client";
+import axios from "axios";
+import { FormEvent, useState } from "react";
+
 const Home = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://test-frontend-dev.onrender.com/login",
+        {
+          username,
+          password,
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Prueba ténica</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
